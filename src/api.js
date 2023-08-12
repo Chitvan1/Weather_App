c0623dc28af70a56b50e6b517e3ffc5cd17b1206
Chitvan Gupta <chitvangupta01@gmail.com>
2023-08-12 13:41:47 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "54ac3a1b58msh2a467bace2780c5p1611edjsn6ea9a4ea7b4e",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "7a770a1f17d08a84c7ef1708435ccd4b";
